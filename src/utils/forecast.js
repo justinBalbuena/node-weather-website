e67c0d0c81b8112c9d9a74e7323816880084b14f
just <justintbalbuena@gmail.com>
2024-10-26 21:30:04 -0400

    Provided latitude and longitude information for user

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,19 +3,18 @@ require('dotenv').config();
 
 const forecast_key = process.env.FORECAST_KEY
 
-const forecast = (longitude, latitude,callback) => {
-  console.log(forecast_key)
+const forecast = (longitude, latitude, callback) => {
   const url = 'https://api.weatherstack.com/current?access_key=' + forecast_key + '&query=' + latitude + ',' + longitude + '&units=f'
   request({ url: url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to weather service", undefined)
     } 
     else if (body.error) {
-      console.log(body)
       callback("Unable to find location", undefined)
     }
     else {
-      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' and feels like ' + body.current.feelslike)
+      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' and feels like ' + body.current.feelslike + 
+        '. The latitude being looked at is: ' + body.location.lat + ' and the longitude being looked at is: ' + body.location.lon)
     }
   })
 }
